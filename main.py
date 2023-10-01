fbd3f15edbc140bd19808d8bbe59fbefd42aad06
miguelhermar <miguelangel.hermar410@gmail.com>
2023-09-30 22:44:41 -0600

    updated

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
 
-from dotenv import load_dotenv
-load_dotenv()  # take environment variables from .env (especially openai api key)
+# from dotenv import load_dotenv
+# load_dotenv()  # take environment variables from .env (especially openai api key)
 
 st.title("News Research Tool 📈")
 st.sidebar.title("News Article URLs")

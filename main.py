3049649bb0fd7267f2a90bc7e5259cdf32b63a55
miguelhermar <miguelangel.hermar410@gmail.com>
2023-10-01 08:48:44 -0600

    handled api keys

--- main.py
+++ main.py
@@ -42,31 +42,35 @@ if api_key:
         main_placeholder.text("Text Splitter...Started...✅✅✅")
         docs = text_splitter.split_documents(data)
         # create embeddings and save it to FAISS index
-        embeddings = OpenAIEmbeddings()
-        vectorstore_openai = FAISS.from_documents(docs, embeddings)
-        main_placeholder.text("Embedding Vector Started Building...✅✅✅")
-        time.sleep(2)
+        try:
+            embeddings = OpenAIEmbeddings()
+            vectorstore_openai = FAISS.from_documents(docs, embeddings)
+            main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+            time.sleep(2)
 
-        # Save the FAISS index to a pickle file
-        with open(file_path, "wb") as f:
-            pickle.dump(vectorstore_openai, f)
+            # Save the FAISS index to a pickle file
+            with open(file_path, "wb") as f:
+                pickle.dump(vectorstore_openai, f)
 
-    query = main_placeholder.text_input("Question: ")
-    if query:
-        if os.path.exists(file_path):
-            with open(file_path, "rb") as f: #read binary file
-                vectorstore = pickle.load(f)
-                chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
-                result = chain({"question": query}, return_only_outputs=True)
-                # result will be a dictionary of this format --> {"answer": "", "sources": [] }
-                st.header("Answer")
-                st.write(result["answer"])
+            query = main_placeholder.text_input("Question: ")
+            if query:
+                if os.path.exists(file_path):
+                    with open(file_path, "rb") as f: #read binary file
+                        vectorstore = pickle.load(f)
+                        chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+                        result = chain({"question": query}, return_only_outputs=True)
+                        # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+                        st.header("Answer")
+                        st.write(result["answer"])
 
-                # Display sources, if available
-                sources = result.get("sources", "")
-                if sources:
-                    st.subheader("Sources:")
-                    sources_list = sources.split("\n")  # Split the sources by newline
-                    for source in sources_list:
-                        st.write(source)
+                        # Display sources, if available
+                        sources = result.get("sources", "")
+                        if sources:
+                            st.subheader("Sources:")
+                            sources_list = sources.split("\n")  # Split the sources by newline
+                            for source in sources_list:
+                                st.write(source)
+        except:
+            main_placeholder.text("Incorrect API key provided.  You can find your API key at https://platform.openai.com/account/api-keys")
+
 
